4beda43dd4d038117c6cdb76e9acd551c2712f9a
AYUSHI CHAWADE <90764085+ayushichawade@users.noreply.github.com>
2024-02-29 22:04:24 +0100

    Update genre.py

--- main code/genre.py
+++ main code/genre.py
@@ -1,17 +1,30 @@
 import pandas as pd
 
-book_features_df = pd.read_excel('Best_Books.xlsx')
-book_interactions_df = pd.read_csv('goodreads_interactions.csv')
 
-df = pd.merge(book_features_df, book_interactions_df, on='book_id', how='left', validate='one_to_many')
+books_df = pd.read_excel('Best_Books.xlsx')
 
-df = df.dropna()
-df = df.drop_duplicates()
 
-popularity_score = df.groupby('book_id').agg({'is_read': 'sum', 'rating': 'mean', 'likedPercent': 'mean', 'numRatings': 'mean'})
-popularity_score['popularity'] = popularity_score['is_read'] / popularity_score['is_read'].sum() + popularity_score['rating'] + popularity_score['likedPercent'] + popularity_score['numRatings']
-popularity_score = popularity_score.sort_values(by='popularity', ascending=False).head(50)
+user_fav_genres = input('Enter your favorite genres (separated by commas): ').split(',')
 
-popular_books = book_features_df.loc[book_features_df['book_id'].isin(popularity_score.index)]
-popular_books = pd.merge(popular_books, popularity_score, on='book_id', how='left').sort_values(by='popularity', ascending=False)
-print(popular_books[['book_id', 'title', 'popularity']])
+
+genre_cols = ['genre 1', 'genre 2', 'genre 3', 'genre 4','genre 5']
+matched_books_df = books_df[books_df[genre_cols].isin(user_fav_genres).any(axis=1)]
+genre_counts = matched_books_df[genre_cols].apply(lambda x: sum(x.isin(user_fav_genres)), axis=1)
+matched_books_df = matched_books_df[genre_counts >= 2].reset_index(drop=True)
+
+
+user_df = pd.read_csv('goodreads_interactions (1).csv')
+user_ratings_df = user_df[user_df['Rating'] >= 4]
+user_rated_book_ids = user_ratings_df['book_id'].unique()
+collab_filtered_books_df = matched_books_df[~matched_books_df['book_id'].isin(user_rated_book_ids)]
+
+
+C = books_df['rating'].mean()
+m = books_df['numRatings'].quantile(0.9)
+collab_filtered_books_df['weighted_score'] = (collab_filtered_books_df['numRatings'] / (collab_filtered_books_df['numRatings'] + m) * collab_filtered_books_df['rating']) + (m / (collab_filtered_books_df['numRatings'] + m) * C)
+
+
+collab_filtered_books_df = collab_filtered_books_df.sort_values('weighted_score', ascending=False).reset_index(drop=True)
+print(f"Recommended books for {', '.join(user_fav_genres)}:\n")
+for i, book in collab_filtered_books_df.head(10).iterrows():
+    print(f"{i+1}. {book['book_id']} ({book['title']}) - {book['weighted_score']:.2f} weighted score, {book['likedPercent']}% liked")
