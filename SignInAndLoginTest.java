0635a7ed5173c994e681c1c9d20f610f1cae0523
AYUSHI CHAWADE <90764085+ayushichawade@users.noreply.github.com>
2024-03-02 19:59:45 +0100

    Update SignInAndLoginTest.java

--- SignInAndLoginTest.java
+++ SignInAndLoginTest.java
@@ -1,24 +1,23 @@
-import static org.junit.Assert.*;
 import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class SignInAndLoginTest {
-    
+
     @Test
     public void testSignUpAndLogin() {
         SignInAndLogin.main(new String[]{});
         String simulatedUserInput = "1\nusername\npassword\n2\nusername\npassword\n3\n";
         System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
-        
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputStream));
-        
+
         SignInAndLogin.main(new String[]{});
         String output = outputStream.toString();
-        
-        assertTrue(output.contains("User registered successfully."));
-        assertTrue(output.contains("Login successful. Welcome, username!"));
+
+        assert output.contains("User registered successfully.") : "User registration message not found";
+        assert output.contains("Login successful. Welcome, username!") : "Login message not found";
     }
 }
